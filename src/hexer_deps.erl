9e78911b86e836c27d7bd539ba18e5fe2d662630
Brujo Benavides <elbrujohalcon@inaka.net>
2016-01-07 18:22:31 -0300

    [#24] Properly get hex DEPS from Makefile

--- src/hexer_deps.erl
+++ src/hexer_deps.erl
@@ -8,11 +8,41 @@
 
 -spec resolve(string()) -> [dep()].
 resolve(AppDir) ->
-  {ok, Makefile} = file:read_file(filename:join([AppDir, "Makefile"])),
-  Regex = "dep_(\\w+) *?= *?hex +([\\w\\d\\.]+)",
-  RegexOpts = [{capture, all_but_first, list}, global, multiline],
-  case re:run(Makefile, Regex, RegexOpts) of
-    {match, Deps} ->
-      [{list_to_atom(Name), Version} || [Name, Version] <- Deps];
-    nomatch -> []
+  HexerMk = filename:join([AppDir, "hexer.mk"]),
+  try
+    create_hexer_mk(HexerMk),
+    Output = os:cmd("cd " ++ AppDir ++ "; make -f hexer.mk list-deps"),
+    DepStrings =
+      [ string:tokens(DepLine, [$=])
+      || DepLine <- string:tokens(Output, [$|, $\n])],
+    [ {to_dep_atom(Dep), hex_version(Fetch)}
+    || [Dep, Fetch] <- DepStrings, is_hex_dep(Fetch)]
+  after
+    _ = file:delete(HexerMk)
   end.
+
+create_hexer_mk(HexerMk) ->
+  Contents =
+    "include Makefile\n"
+    "\n"
+    "THE_DEPS = "
+      "$(foreach dep,"
+        "$(filter-out $(IGNORE_DEPS),$(BUILD_DEPS) $(DEPS)),"
+        "$(dep) = $(dep_$(dep))\\|)\n"
+    "\n"
+    "list-deps:\n"
+    "\t@echo $(THE_DEPS)\n",
+  ok = file:write_file(HexerMk, Contents).
+
+is_hex_dep(Fetch) ->
+  case string:tokens(Fetch, [$\s, $\t]) of
+    ["hex" | _] -> true;
+    _ -> false
+  end.
+
+hex_version(Fetch) ->
+  ["hex", Version | _] = string:tokens(Fetch, [$\s, $\t]),
+  Version.
+
+to_dep_atom(Dep) ->
+  list_to_atom([Char || Char <- Dep, Char /= $\s]).
